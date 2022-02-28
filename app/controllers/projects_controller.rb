ff44a535b7053866895792f64b260c1737a26d14
Cesare Ferrari <cesare@maryandferrari.com>
2022-02-27 21:09:18 -0500

    Add new project functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,38 @@
 class ProjectsController < ApplicationController
+  before_action :set_user
   before_action :require_signin
 
   def index
-    @user = User.find(params[:user_id])
     @projects = @user.projects
   end
 
   def show
-    @user = User.find(params[:user_id])
     @project = @user.projects.find(params[:id])
+  end
+
+  def new
+    @project = @user.projects.new()
+  end
+
+  def create
+    @project = @user.projects.new(project_params)
+
+    if @project.save
+      flash[:notice] = "Project created successfully."
+      redirect_to user_projects_url(@user)
+    else
+      flash.now[:alert] = "Please fix the errors below."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
