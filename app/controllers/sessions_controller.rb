e910ac817e0509061bda30395d430e3676ab6403
Cesare Ferrari <cesare@maryandferrari.com>
2022-03-03 08:46:48 -0500

    Fix redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_projects_url(user), notice: "Welcome back."
+      redirect_to projects_url, notice: "Welcome back."
     else
       flash.now[:alert] = "Invalid email/password"
       render :new, status: :unprocessable_entity
