9ea6dfc19667e426a13c2e3add91876d9c4765db
Cesare Ferrari <cesare@maryandferrari.com>
2022-03-08 10:57:37 -0500

    Style project list

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,6 +1,6 @@
 module ProjectsHelper
   def status_for(project)
-    classes = %w(inline-block mb-4 px-2 py-1 text-xs uppercase text-white bg-indigo-600 rounded-sm)
+    classes = %w(inline-block px-2 py-1 text-xs uppercase text-white bg-indigo-600 rounded-sm)
     if project.done?
       content_tag(:span, "Done", class: "#{classes.join(" ")} bg-indigo-600")
     else
