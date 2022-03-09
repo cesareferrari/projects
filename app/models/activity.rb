ae3aeb81c434fc7fa590f0f029a1d6270bcc839d
Cesare Ferrari <cesare@maryandferrari.com>
2022-03-09 10:06:13 -0500

    Order done activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,5 +5,5 @@ class Activity < ApplicationRecord
   has_rich_text :content
 
   scope :todo, -> { where(done: false).order(next: :desc) }
-  scope :done, -> { where(done: true) }
+  scope :done, -> { where(done: true).order(updated_at: :desc) }
 end
