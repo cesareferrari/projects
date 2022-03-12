abfd66f358a0d186219cd20e5042bf184797cfdf
Cesare Ferrari <cesare@maryandferrari.com>
2022-03-11 21:16:46 -0500

    Helpers for date and priority project

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -7,4 +7,22 @@ module ProjectsHelper
       content_tag(:span, "Active", class: "#{classes.join(" ")} bg-green-600")
     end
   end
+
+  def date_for(project)
+    if project.done?
+      return content_tag(:span,
+                       "done on #{project.updated_at.to_s(:default)}",
+                        class: "uppercase text-xs")
+    end
+
+    content_tag(:span, project.created_at.to_s(:default),
+                class: "uppercase text-xs")
+  end
+
+  def priority_for(project)
+    unless project.done?
+      content_tag(:div, project.priority,
+              class: "w-6 h-6 p-2 rounded-full bg-indigo-400 flex justify-center items-center text-white font-bold text-sm" )
+    end
+  end
 end
