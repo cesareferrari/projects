fc594b3a15abcd2911f93c62e1540a0a5c76fd20
Cesare Ferrari <cesare@maryandferrari.com>
2022-03-28 10:00:50 -0400

    Redirect to user show on signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to projects_url, notice: "Welcome back."
+      redirect_to user, notice: "Welcome back."
     else
       flash.now[:alert] = "Invalid email/password"
       render :new, status: :unprocessable_entity
