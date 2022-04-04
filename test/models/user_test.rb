32980525feb8ac1678790d6e907a3c510d13b8d0
Cesare Ferrari <cesare@maryandferrari.com>
2022-04-04 10:19:02 -0400

    Add user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,23 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test 'valid user' do
+  def setup
+    @user = users(:cesare) # from the fixtures
   end
 
-  test 'invalid without a name' do
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'invalid without name' do
+    @user.name = nil
+    refute @user.valid?, 'user is valid without a name'
+    assert_not_nil @user.errors[:name], 'no validation error for name present'
   end
 
   test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?, 'user is valid without an email'
+    assert_not_nil @user.errors[:email], 'no validation error for email present'
   end
 end
